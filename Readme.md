4afa85daeacfe6565f4e9423af670393e944ce05
Amanda Santos <amanda.s.soares@hotmail.com>
2022-10-15 09:54:54 -0300

    first commit

---
+++ Readme.md
@@ -0,0 +1,11 @@
+# Conversor de Moedas
+
+Projeto desenvolvimento na imersão Alura.
+
+Praticando HTML, CSS e Javascript.
+
+Neste projeto aprendi a interagir com prompt HTML e JavaScript, passando dados entre a página e o arquivo javascript.
+<br>
+<br>
+
+![preview](./preview.png)
---
+++ scripts.js
@@ -0,0 +1,16 @@
+var valorEmDolarTexto = prompt("Qual é o valor dolar que você quer converter?")
+
+var valorEmDolarNumero = parseFloat(valorEmDolarTexto)
+
+var valorEmReal = valorEmDolarNumero * 5.50
+
+var valorEmRealFixado = valorEmReal.toFixed(2)
+
+alert(valorEmRealFixado)
+
+//Revisão
+//variáveis var int- float - string
+//alert -parseInt- parsefloat- prompt
+//operações + somar * multiplicar
+// comentário final
+
